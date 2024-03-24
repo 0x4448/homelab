258126919dd33c40c37de6d743b7c0f00e95360b
0x4448 <86135470+0x4448@users.noreply.github.com>
2024-03-24 01:57:11 +0000

    feat: init script

---
+++ init.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -eu
+
+branch="main"
+
+while getopts ":b:h" opt; do
+  case $opt in
+    b)
+      branch="$OPTARG"
+      ;;
+    h)
+      echo "Usage: init.sh [OPTION]..."
+      echo "Initialize a node."
+      echo
+      echo "  -b  git branch to use"
+      echo "  -h  display this help and exit"
+      exit 0
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      ;;
+    ?)
+      echo "init.sh: invalid option -$OPTARG"
+      echo "Try 'init.sh -h' for more information."
+      exit 1
+      ;;
+  esac
+done
+
+
+workDir=$(pwd)
+repoDir=$(mktemp -d)
+
+function cleanup {
+  cd "$workDir"
+  rm -rf "$repoDir"
+}
+
+trap cleanup EXIT
+
+apt-get update
+apt-get install --no-install-recommends --yes git pipx
+
+export PIPX_HOME=/usr/local/pipx
+export PIPX_BIN_DIR=/usr/local/bin
+pipx install ansible-core
+
+cd "$repoDir" || exit 1
+git clone "https://github.com/0x4448/homelab" .
+git switch "$branch"
+ansible-playbook --become playbook.yml
